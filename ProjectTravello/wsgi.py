f1541126ef83b5b8a51abc49ce445e3c8548da9a
Sandhiya <sandhiyaganesan18@gmail.com>
2025-06-29 19:54:43 +0530

    Add files

---
+++ ProjectTravello/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ProjectTravello project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProjectTravello.settings")
+
+application = get_wsgi_application()
